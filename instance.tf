1f19ceb6def23f189f3ae8339d478306a0ad2110
usmanthotti12 <98163236+usmanthotti12@users.noreply.github.com>
2022-02-04 17:17:06 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -50,7 +50,7 @@ variable "num_paravirtualized_volumes_per_instance" {
 }
 
 variable "instance_shape" {
-  default = "VM.Standard2.1"
+  default = "VM.Standard.E4.Flex"
 }
 
 variable "instance_ocpus" {
