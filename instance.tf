a64ed471778e91925b7b59fa57f3b166ed273caf
usmanthotti12 <98163236+usmanthotti12@users.noreply.github.com>
2022-02-07 20:29:47 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -41,13 +41,7 @@ variable "num_instances" {
 # Defines the number of volumes to create and attach to each instance
 # NOTE: Changing this value after applying it could result in re-attaching existing volumes to different instances.
 # This is a result of using 'count' variables to specify the volume and instance IDs for the volume attachment resource.
-variable "num_iscsi_volumes_per_instance" {
-  default = "1"
-}
 
-variable "num_paravirtualized_volumes_per_instance" {
-  default = "2"
-}
 
 variable "instance_shape" {
   default = "VM.Standard.E4.Flex"
@@ -61,18 +55,7 @@ variable "instance_shape_config_memory_in_gbs" {
   default = 1
 }
 
-# variable "instance_image_ocid" {
-#   type = map(string)
 
-#   default = {
-#     # See https://docs.us-phoenix-1.oraclecloud.com/images/
-#     # Oracle-provided image "Oracle-Linux-7.5-2018.10.16-0"
-#     us-phoenix-1   = "ocid1.image.oc1.phx.aaaaaaaaoqj42sokaoh42l76wsyhn3k2beuntrh5maj3gmgmzeyr55zzrwwa"
-#     us-ashburn-1   = "ocid1.image.oc1.iad.aaaaaaaageeenzyuxgia726xur4ztaoxbxyjlxogdhreu3ngfj2gji3bayda"
-#     eu-frankfurt-1 = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaaitzn6tdyjer7jl34h2ujz74jwy5nkbukbh55ekp6oyzwrtfa4zma"
-#     uk-london-1    = "ocid1.image.oc1.uk-london-1.aaaaaaaa32voyikkkzfxyo4xbdmadc2dmvorfxxgdhpnk6dw64fa3l4jh7wa"
-#   }
-# }
 
 variable "flex_instance_image_ocid" {
   type = map(string)
@@ -83,9 +66,7 @@ variable "flex_instance_image_ocid" {
   }
 }
 
-variable "db_size" {
-  default = "50" # size in GBs
-}
+
 
 variable "tag_namespace_description" {
   default = "Just a test"
@@ -153,82 +134,9 @@ resource "oci_core_instance" "test_instance" {
   }
 }
 
-# Define the volumes that are attached to the compute instances.
-
-resource "oci_core_volume" "test_block_volume" {
-  count               = var.num_instances * var.num_iscsi_volumes_per_instance
-  availability_domain = data.oci_identity_availability_domain.ad.name
-  compartment_id      = var.compartment_ocid
-  display_name        = "TestBlock-usman${count.index}"
-  size_in_gbs         = var.db_size
-}
 
 
 
-resource "oci_core_volume_attachment" "test_block_attach" {
-  count           = var.num_instances * var.num_iscsi_volumes_per_instance
-  attachment_type = "iscsi"
-  instance_id     = oci_core_instance.test_instance[floor(count.index / var.num_iscsi_volumes_per_instance)].id
-  volume_id       = oci_core_volume.test_block_volume[count.index].id
-  device          = count.index == 0 ? "/dev/oracleoci/oraclevdb" : ""
-
-  # Set this to enable CHAP authentication for an ISCSI volume attachment. The oci_core_volume_attachment resource will
-  # contain the CHAP authentication details via the "chap_secret" and "chap_username" attributes.
-  use_chap = true
-  # Set this to attach the volume as read-only.
-  #is_read_only = true
-}
-
-resource "oci_core_volume" "test_block_volume_paravirtualized" {
-  count               = var.num_instances * var.num_paravirtualized_volumes_per_instance
-  availability_domain = data.oci_identity_availability_domain.ad.name
-  compartment_id      = var.compartment_ocid
-  display_name        = "TestBlockParavirtualized-usman${count.index}"
-  size_in_gbs         = var.db_size
-}
-
-resource "oci_core_volume_attachment" "test_block_volume_attach_paravirtualized" {
-  count           = var.num_instances * var.num_paravirtualized_volumes_per_instance
-  attachment_type = "paravirtualized"
-  instance_id     = oci_core_instance.test_instance[floor(count.index / var.num_paravirtualized_volumes_per_instance)].id
-  volume_id       = oci_core_volume.test_block_volume_paravirtualized[count.index].id
-  # Set this to attach the volume as read-only.
-  #is_read_only = true
-}
-
-resource "oci_core_volume_backup_policy_assignment" "policy" {
-  count     = var.num_instances
-  asset_id  = oci_core_instance.test_instance[count.index].boot_volume_id
-  policy_id = data.oci_core_volume_backup_policies.test_predefined_volume_backup_policies.volume_backup_policies[0].id
-}
-
-# resource "null_resource" "remote-exec" {
-#   depends_on = [
-#     oci_core_instance.test_instance,
-#     oci_core_volume_attachment.test_block_attach,
-#   ]
-#   count = var.num_instances * var.num_iscsi_volumes_per_instance
-
-#   provisioner "remote-exec" {
-#     connection {
-#       agent       = false
-#       timeout     = "30m"
-#       host        = oci_core_instance.test_instance[count.index % var.num_instances].public_ip
-#       user        = "opc"
-#       private_key = var.ssh_private_key
-# }
-
-#     inline = [
-#       "touch ~/IMadeAFile.Right.Here",
-#       "sudo iscsiadm -m node -o new -T ${oci_core_volume_attachment.test_block_attach[count.index].iqn} -p ${oci_core_volume_attachment.test_block_attach[count.index].ipv4}:${oci_core_volume_attachment.test_block_attach[count.index].port}",
-#       "sudo iscsiadm -m node -o update -T ${oci_core_volume_attachment.test_block_attach[count.index].iqn} -n node.startup -v automatic",
-#       "sudo iscsiadm -m node -T ${oci_core_volume_attachment.test_block_attach[count.index].iqn} -p ${oci_core_volume_attachment.test_block_attach[count.index].ipv4}:${oci_core_volume_attachment.test_block_attach[count.index].port} -o update -n node.session.auth.authmethod -v CHAP",
-#       "sudo iscsiadm -m node -T ${oci_core_volume_attachment.test_block_attach[count.index].iqn} -p ${oci_core_volume_attachment.test_block_attach[count.index].ipv4}:${oci_core_volume_attachment.test_block_attach[count.index].port} -o update -n node.session.auth.username -v ${oci_core_volume_attachment.test_block_attach[count.index].chap_username}",
-#       "sudo iscsiadm -m node -T ${oci_core_volume_attachment.test_block_attach[count.index].iqn} -p ${oci_core_volume_attachment.test_block_attach[count.index].ipv4}:${oci_core_volume_attachment.test_block_attach[count.index].port} -o update -n node.session.auth.password -v ${oci_core_volume_attachment.test_block_attach[count.index].chap_secret}",
-#       "sudo iscsiadm -m node -T ${oci_core_volume_attachment.test_block_attach[count.index].iqn} -p ${oci_core_volume_attachment.test_block_attach[count.index].ipv4}:${oci_core_volume_attachment.test_block_attach[count.index].port} -l",
-#     ]
-#   }
-# }
 
 /*
 # Gets the boot volume attachments for each instance
@@ -291,11 +199,7 @@ output "silver_policy_id" {
   value = data.oci_core_volume_backup_policies.test_predefined_volume_backup_policies.volume_backup_policies[0].id
 }
 
-/*
-output "attachment_instance_id" {
-  value = data.oci_core_boot_volume_attachments.test_boot_volume_attachments.*.instance_id
-}
-*/
+
 
 resource "oci_core_vcn" "test_vcn" {
   cidr_block     = "10.1.0.0/16"
