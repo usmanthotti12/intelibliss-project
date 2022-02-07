4df7eea4fc97a236ef165d18cf52abb2c764c4b2
usmanthotti12 <98163236+usmanthotti12@users.noreply.github.com>
2022-02-07 20:12:51 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -61,18 +61,18 @@ variable "instance_shape_config_memory_in_gbs" {
   default = 1
 }
 
-variable "instance_image_ocid" {
-  type = map(string)
+# variable "instance_image_ocid" {
+#   type = map(string)
 
-  default = {
-    # See https://docs.us-phoenix-1.oraclecloud.com/images/
-    # Oracle-provided image "Oracle-Linux-7.5-2018.10.16-0"
-    us-phoenix-1   = "ocid1.image.oc1.phx.aaaaaaaaoqj42sokaoh42l76wsyhn3k2beuntrh5maj3gmgmzeyr55zzrwwa"
-    us-ashburn-1   = "ocid1.image.oc1.iad.aaaaaaaageeenzyuxgia726xur4ztaoxbxyjlxogdhreu3ngfj2gji3bayda"
-    eu-frankfurt-1 = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaaitzn6tdyjer7jl34h2ujz74jwy5nkbukbh55ekp6oyzwrtfa4zma"
-    uk-london-1    = "ocid1.image.oc1.uk-london-1.aaaaaaaa32voyikkkzfxyo4xbdmadc2dmvorfxxgdhpnk6dw64fa3l4jh7wa"
-  }
-}
+#   default = {
+#     # See https://docs.us-phoenix-1.oraclecloud.com/images/
+#     # Oracle-provided image "Oracle-Linux-7.5-2018.10.16-0"
+#     us-phoenix-1   = "ocid1.image.oc1.phx.aaaaaaaaoqj42sokaoh42l76wsyhn3k2beuntrh5maj3gmgmzeyr55zzrwwa"
+#     us-ashburn-1   = "ocid1.image.oc1.iad.aaaaaaaageeenzyuxgia726xur4ztaoxbxyjlxogdhreu3ngfj2gji3bayda"
+#     eu-frankfurt-1 = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaaitzn6tdyjer7jl34h2ujz74jwy5nkbukbh55ekp6oyzwrtfa4zma"
+#     uk-london-1    = "ocid1.image.oc1.uk-london-1.aaaaaaaa32voyikkkzfxyo4xbdmadc2dmvorfxxgdhpnk6dw64fa3l4jh7wa"
+#   }
+# }
 
 variable "flex_instance_image_ocid" {
   type = map(string)
